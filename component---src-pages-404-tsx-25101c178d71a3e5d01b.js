d9992553cb7c71b26a9aa67cf3b7d5ee21b4839f
cieloazul310 <cieloazul310@gmail.com>
2020-06-02 14:20:51 +0900

    Updates

---
+++ component---src-pages-404-tsx-25101c178d71a3e5d01b.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{"i6+/":function(t,e,a){"use strict";a.r(e);var n=a("mXGw"),o=a("/ZiB"),r=a("JF+v"),i=a("ThN4");e.default=function(){var t=Object(r.useLocation)();return n.createElement(i.a,{title:"Not Found",maxWidth:"md"},n.createElement(o.a,{variant:"h2",gutterBottom:!0},"NOT FOUND"),n.createElement(o.a,{variant:"h5",gutterBottom:!0},n.createElement("code",null,t.pathname)," doesn't exist."))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-25101c178d71a3e5d01b.js.map
